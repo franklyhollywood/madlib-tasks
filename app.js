5d96bd00597d161edeed1d530a3b90e93c82dc22
Arnaud Douglas Ardans II <github@arnaudardans.com>
2021-02-12 15:41:34 -0800

    timer up: adding javascript

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 // import functions and grab DOM elements
+const input1 = document.getElementById('verb1');
+const input2 = document.getElementById('era');
+const input3 = document.getElementById('adjective1');
+const input4 = document.getElementById('name1');
+const input5 = document.getElementById('noun1');
+const input6 = document.getElementById('adj-adj-noun');
+const input7 = document.getElementById('adjective2');
+const input8 = document.getElementById('verb-in-action');
+const input9 = document.getElementById('verb-in-action2');
+const input10 = document.getElementById('adjective3');
+const button = document.getElementById('button-madlib');
+
+const output1 = document.getElementById('span-verb1');
+const output2 = document.getElementById('span-era');
+const output3 = document.getElementById('span-adjective1');
+const output4 = document.getElementById('span-name1');
+const output5 = document.getElementById('span-noun1');
+const output6 = document.getElementById('span-adj-adj-noun');
+const output7 = document.getElementById('span-adjective2');
+const output8 = document.getElementById('span-verb-in-action');
+const output9 = document.getElementById('span-verb-in-action2');
+const output10 = document.getElementById('span-adjective3');
+
 
 // initialize state
+button.addEventListener('click', () => {
+    // result.textContent = add(Number(input1.value), Number(input2.value));
+    
+});
+
 
 // set event listeners to update state and DOM
 
-const num1 = document.getElementById('input1');
-const num2 = document.getElementById('input2');
-const btn = document.getElementById('button-add');
-const result = document.getElementById('result1');
 
 
-btn.addEventListener('click', () => {
-    result.textContent = add(Number(input1.value), Number(input2.value));
-    //num1.value = '';
-    //num2.value = '';
-});
+
+
