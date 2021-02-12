e5ce37ba39f0065d83f7f74cf1bd17d58485b1ef
franklyhollywood <github@srvsync.com>
2021-02-12 15:23:33 -0800

    added placeholder and js script to index

--- app.js
+++ app.js
@@ -3,3 +3,15 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const num1 = document.getElementById('input1');
+const num2 = document.getElementById('input2');
+const btn = document.getElementById('button-add');
+const result = document.getElementById('result1');
+
+
+btn.addEventListener('click', () => {
+    result.textContent = add(Number(input1.value), Number(input2.value));
+    //num1.value = '';
+    //num2.value = '';
+});
